dea7aad667d5da69fefb4a99b780d66d0a7317e1
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-01 12:54:28 +0530

    Add files via upload

---
+++ practice questions/divide.cpp
@@ -0,0 +1,11 @@
+// ~mohd-uzaif-ansari
+#include <iostream>
+using namespace std;
+    int main(){
+        float a, b, divide;   // float becaus it need decimal part also
+        a=5;
+        b=2;
+        divide = a/b;
+        cout << "Divide = " << (divide);
+        return 0;
+    }
