2a61d98a08d488c86b0b04a8cca80215de1da723
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-01 12:54:05 +0530

    Add files via upload

---
+++ practice questions/product.cpp
@@ -0,0 +1,10 @@
+// ~mohd-uzaif-ansari
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10, b = 4, product;
+    product = a * b;
+    cout << "sum = " << (product);
+    return 0;
+}
