f0611ddf2cd29f771ccd691ba11c2eba96b711f6
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-01 12:54:56 +0530

    Add files via upload

---
+++ practice questions/operations_in.cpp
@@ -0,0 +1,21 @@
+// ~mohd-uzaif-ansari
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, add, minus, prod, divide, modulu;
+    a = 16;
+    b = 4;
+    add = a + b;
+    minus = a - b;
+    prod = a * b;
+    divide = a / b;
+    modulu = a % b;
+    cout << "Add = " << (add) << "\n";
+    cout << "minus = " << (minus) << "\n";
+    cout << "prod = " << (prod) << "\n";
+    cout << "divide = " << (divide) << "\n";
+    cout << "divide = " << (divide) << "\n";
+    cout << "modulu = " << (modulu) << "\n";
+    return 0;
+}
