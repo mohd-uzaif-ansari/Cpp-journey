85690a46c02e6e13c7f7b9e502d4c9c32fd67de2
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-03 11:22:19 +0530

    Add files via upload

---
+++ practice questions/cheaking_oddeven.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 4;
+    if (a % 2 == 0)
+    {
+        cout << "EVEN N U M B E R\n";
+        cout << "Your number is " << (a);
+    }
+    else
+    {
+        cout << "ODD N U M B E R";
+    }
+    return 0;
+}
