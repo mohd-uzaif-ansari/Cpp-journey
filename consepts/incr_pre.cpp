1eb77241dc9dd900da606c317e3dd26f0d8324e3
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-04 23:25:36 +0530

    Add files via upload

---
+++ consepts/incr_pre.cpp
@@ -0,0 +1,12 @@
+// ~mohd-uzaif-ansari
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    a = 10;
+    b = ++a; // update> kaam
+    cout << "b = " << (b) << "\n";
+    cout << "a = " << (a);
+    return 0;
+}
