735fe6110e5d76ddd5c0d8ffc8446693b284dde4
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-04 23:21:35 +0530

    Add files via upload

---
+++ consepts/if.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 26;
+    if (a >= 18)
+    {
+        cout << "Eligible to drive\n";
+        cout << "your age is " << (a);
+    }
+
+    return 0;
+}
