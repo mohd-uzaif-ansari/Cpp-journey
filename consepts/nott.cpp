5d78bcede086e5dcf959a49197d181285318f308
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-05 23:36:14 +0530

    Add files via upload

---
+++ consepts/nott.cpp
@@ -0,0 +1,12 @@
+// ~mohd-uzaif-ansari
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    a = 11;
+    b = 9;
+        cout << (a<b) << "\n"; //without using not
+        cout << !(a<b); //with using not
+    return 0;
+}
