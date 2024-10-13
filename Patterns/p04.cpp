48bab2725195fade96c541dbb28d6fea7562b531
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-13 22:31:32 +0530

    Add files via upload

---
+++ Patterns/p04.cpp
@@ -0,0 +1,16 @@
+// ~ mohd-uzaif-ansari
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5; // Number of rows
+    int num = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
