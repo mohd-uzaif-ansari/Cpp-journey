831671669aad8dd7fc3e691780a992a7175d1ef3
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-16 21:53:28 +0530

    Add files via upload

--- consepts/p01.cpp
+++ consepts/p01.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
+    int sum(int n){
+        int sum = 0;
 
-int prthelo(){
-    cout << "Mohammad Uzaif Ansari" << endl;
-    return 5 ;
-}
+        for(int i = 1; i<=n; i++){
+            sum +=i;
+        }
+        return sum;
+    }
     int main(){
-        cout << prthelo() << endl;
+        cout << sum(5) << endl;
+        cout << sum(10) << endl;
+        cout << sum(15) << endl;
         return 0;
     }
