f89fb664100364c9ee060ecaf94f6371bc59c7f0
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-16 21:52:00 +0530

    Add files via upload

---
+++ consepts/p01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int prthelo(){
+    cout << "Mohammad Uzaif Ansari" << endl;
+    return 5 ;
+}
+    int main(){
+        cout << prthelo() << endl;
+        return 0;
+    }
