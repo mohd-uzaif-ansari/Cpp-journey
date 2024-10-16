055cb46d3be89032e868e1ea50bdf92569ec5674
Mohd Uzaif Ansari <mohammaduzaif0111@gmail.com>
2024-10-16 21:51:42 +0530

    Add files via upload

---
+++ consepts/hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void displayhello()
+{
+    cout << "Hello World !" <<endl;
+}
+int main()
+{
+    displayhello();
+    displayhello();
+    displayhello();
+    displayhello();
+    displayhello();
+    displayhello();
+    displayhello();
+    displayhello();
+    displayhello();
+    return 0;
+}
